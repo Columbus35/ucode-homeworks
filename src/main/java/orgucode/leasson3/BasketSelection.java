9cfdee4f01bb86ed5edd4820f6f067c5ba428225
Levei Edmond <levei_edmond@yahoo.com>
2019-12-12 19:32:29 +0200

    First commit

---
+++ src/main/java/orgucode/leasson3/BasketSelection.java
@@ -0,0 +1,20 @@
+package orgucode.leasson3;
+
+public class BasketSelection {
+    public static void main(String[] args) {
+        Student one1 = new Student ("Markus", 18, 180);
+        one1.name= "Markus";
+        one1.alter= 18;
+        one1.hohe= 180;
+        if(one1.hohe>=180){
+            System.out.println(one1.name);}
+
+
+
+
+
+    }
+
+
+
+}
---
+++ src/main/java/orgucode/leasson3/Student.java
@@ -0,0 +1,14 @@
+package orgucode.leasson3;
+
+public class Student {
+
+    String name;
+    int alter;
+    int hohe;
+
+    Student(String name, int alter, int hohe){
+
+
+    }
+
+}
